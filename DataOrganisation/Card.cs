24286df0ea3bfd85e5ba71c1c964b43bb8564885
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-08-24 18:37:58 +0100

    Add project files.

---
+++ DataOrganisation/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataOrganisation
+{
+    class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+    }
+}
---
+++ DataOrganisation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataOrganisation
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Card randomCard = new Card((Values)random.Next(1,14), (Suits)random.Next(4));
+            Console.WriteLine(randomCard.Name);
+        }
+    }
+}
